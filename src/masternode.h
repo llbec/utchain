552cf19dfff8cce19b5f6827bc85f35265b300b2
bicliu <klins@163.com>
2018-10-22 16:38:17 +0800

    update

--- src/masternode.h
+++ src/masternode.h
@@ -309,7 +309,7 @@ public:
 
     int GetCollateralAge();
 
-    int GetLastPaidTime() { return nTimeLastPaid; }
+    int64_t GetLastPaidTime() { return nTimeLastPaid; }
     int GetLastPaidBlock() { return nBlockLastPaid; }
     void UpdateLastPaid(const CBlockIndex *pindex, int nMaxBlocksToScanBack);
 
